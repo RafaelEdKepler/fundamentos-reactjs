d2b685fdb8ee27d71898e9ce9b42f07458d601ca
Rafael Kepler <rafael.kepler@hotmail.com>
2024-05-10 18:11:56 -0300

    New comment section included in the Post component, still miss the comment list section

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,6 +32,18 @@ export function Post() {
         <p>👉 <a href="#">jane.design/doctorcare</a></p>
         <p><a href="#">#novoprojeto</a> <a href="#">#nlw</a> <a href="#">#rocketseat</a></p>
       </div>
+
+      <form
+        className={styles.commentForm}
+      >
+        <strong>Deixe um comentário</strong>
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
