871b44b127cd5a18f3e4634a5724eca7e4452c2f
Rafael Kepler <rafael.kepler@hotmail.com>
2024-05-16 18:12:04 -0300

    Included props on post section

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,69 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/rafaeledkepler.png",
+      name: "Rafael Eduardo Kepler",
+      role: "Frontend Developer at @Compass.UOL",
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋'
+      },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare'
+      }
+    ],
+    publishedAt: new Date('2024-05-14 13:24')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://scontent.fgel3-1.fna.fbcdn.net/v/t39.30808-6/309895457_10225927622790669_6948157515261522626_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGA5ncK6FxDRxrgueohCGp0aC9aWXExiuloL1pZcTGK6Q2-Hwc5aDuGKYagFD9tXdEzOcjGdxDLpyzNEGvkYq_W&_nc_ohc=9TtkvWHcDYAQ7kNvgFfaAhi&_nc_ht=scontent.fgel3-1.fna&oh=00_AYC3qqxVrUFoQ9fRYFh3w6Je0e9oUHiQfB-lYWUG4hIAkg&oe=6649522F",
+      name: "Thaysa Kaestner Kopsch",
+      role: "Missionária at @MEUCPANAMBI",
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋'
+      },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare'
+      }
+    ],
+    publishedAt: new Date('2024-05-12 20:24')
+  },
+]
+
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
       <div className={styles.wrapper}>
-        <Sidebar/>
+        <Sidebar />
         <main>
-          <Post/>
-          <Post/>
+          {posts.map(post => (
+            <Post key={post.id} author={post.author} content={post.content} publishedAt={post.publishedAt} />
+          ))}
         </main>
       </div>
     </>
   )
 }
 
-export {App}
+export { App }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,34 +1,57 @@
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR"
+
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
+
 import styles from "./Post.module.css";
 
-export function Post() {
+export function Post({ content, author, publishedAt }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  })
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div
           className={styles.author}
         >
-          <Avatar src="https://github.com/rafaeledkepler.png"/>
+          <Avatar src={author.avatarUrl} />
           <div
             className={styles.authorInfo}
           >
-            <strong>Rafael Eduardo Kepler</strong>
-            <span>Web developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
         <time
-          title="Dez de Maio às 17:38"
-          dateTime="2024-05-10 17:37"
-        >Publicado há 1h</time>
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
       </header>
       <div
         className={styles.content}
       >
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉 <a href="#">jane.design/doctorcare</a></p>
-        <p><a href="#">#novoprojeto</a> <a href="#">#nlw</a> <a href="#">#rocketseat</a></p>
+        {
+          content.map(line => {
+            if (line.type === 'paragraph') {
+              return <p>{line.content}</p>
+            } else if (line.type === 'link') {
+              return (
+                <p>
+                  <a href="#">{line.content}</a>
+                </p>
+              )
+            }
+          })
+        }
       </div>
 
       <form
@@ -43,9 +66,7 @@ export function Post() {
         </footer>
       </form>
       <div className={styles.commentList}>
-        <Comment/>
-        <Comment/>
-        <Comment/>
+        <Comment />
       </div>
     </article>
   )
